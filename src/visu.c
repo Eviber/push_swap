c7389d8efcb42a31910e8ff2be3b7ee942494345
Eviber <youv.didj@gmail.com>
2017-10-28 05:09:02 +0200

    Almost there...

--- src/visu.c
+++ src/visu.c
@@ -6,7 +6,7 @@
 /*   By: ygaude <ygaude@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/26 06:57:16 by ygaude            #+#    #+#             */
-/*   Updated: 2017/10/27 04:39:14 by ygaude           ###   ########.fr       */
+/*   Updated: 2017/10/28 04:08:04 by ygaude           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include "push_swap.h"
 
 # define WIN_W 1920
-# define WIN_H 1080
+# define WIN_H 1000
 
 typedef struct			s_winenv
 {
@@ -25,13 +25,6 @@ typedef struct			s_winenv
 	SDL_Window			*win;
 	SDL_Renderer		*render;
 }						t_winenv;
-
-typedef struct			s_order
-{
-	struct s_order		*next;
-	struct s_order		*last;
-	char				order;
-}						t_order;
 
 t_winenv				*getsdlenv(void)
 {
@@ -86,25 +79,53 @@ static void		clear(t_winenv *env)
 	SDL_RenderFillRect(env->render, &rect);
 }
 
-static void		displaypile(t_winenv *env, t_pile *p, SDL_Rect rect)
+unsigned int	ft_abs(int n)
+{
+	return ((unsigned int)((n < 0) ? -n : n));
+}
+
+static unsigned int	maxvalue(t_pile *p, int *pop)
 {
 	t_pile			*cur;
+	unsigned int	max;
+
+	max = ft_abs(p->n);
+	cur = p->next;
+	*pop = 1;
+	while (cur != p)
+	{
+		if (ft_abs(cur->n) > max)
+			max = ft_abs(cur->n);
+		cur = cur->next;
+		(*pop)++;
+	}
+	return (max);
+}
+
+static void		displaypile(t_winenv *env, t_pile *p, SDL_Rect rect, unsigned int max, int pop)
+{
+	t_pile			*cur;
+	int				orix;
 
 	if (!p)
 		return ;
+	orix = rect.x;
 	cur = p->last;
+	rect.h = WIN_H / pop;
 	while (cur && cur != p)
 	{
-		rect.w = 10 * cur->n;
+		rect.y -= rect.h;
+		rect.w = WIN_W / 2 * (ft_abs(cur->n) + 1) / max;
+		rect.x = orix - (rect.w / 2);
 		if (cur->n < 0)
 			SDL_SetRenderDrawColor(env->render, 255, 255, 255, 0);
 		else
 			SDL_SetRenderDrawColor(env->render, 0, 0, 0, 0);
 		SDL_RenderFillRect(env->render, &rect);
 		cur = cur->last;
-		rect.y -= rect.h;
 	}
-	rect.w = 10 * cur->n;
+	rect.w = WIN_W / 2 * (ft_abs(cur->n) + 1) / max;
+	rect.x = orix - (rect.w / 2);
 	if (cur->n < 0)
 		SDL_SetRenderDrawColor(env->render, 255, 255, 255, 0);
 	else
@@ -112,48 +133,21 @@ static void		displaypile(t_winenv *env, t_pile *p, SDL_Rect rect)
 	SDL_RenderFillRect(env->render, &rect);
 }
 
-static void		displaystatus(t_piles p)
+static void		displaystatus(t_piles p, unsigned int max, int pop)
 {
 	t_winenv 	*env;
 	SDL_Rect	rect;
 
 	env = getsdlenv();
 	clear(env);
-	rect.w = 10;
-	rect.h = 10;
+	rect.w = 1;
+	rect.h = 1;
 	rect.y = WIN_H - rect.h;
 	rect.x = WIN_W / 4;
-	displaypile(env, *(p.p1), rect);
+	displaypile(env, *(p.p1), rect, max, pop);
 	rect.x = 3 * WIN_W / 4;
-	displaypile(env, *(p.p2), rect);
+	displaypile(env, *(p.p2), rect, max, pop);
 	SDL_RenderPresent(env->render);
-}
-
-static char		strton(char *str)
-{
-	if (ft_strequ(str, "pa"))
-		return (PA);
-	if (ft_strequ(str, "pb"))
-		return (PB);
-	if (ft_strequ(str, "sa"))
-		return (SA);
-	if (ft_strequ(str, "sb"))
-		return (SB);
-	if (ft_strequ(str, "ss"))
-		return (SS);
-	if (ft_strequ(str, "ra"))
-		return (RA);
-	if (ft_strequ(str, "rb"))
-		return (RB);
-	if (ft_strequ(str, "rr"))
-		return (RR);
-	if (ft_strequ(str, "rra"))
-		return (RRA);
-	if (ft_strequ(str, "rrb"))
-		return (RRB);
-	if (ft_strequ(str, "rrr"))
-		return (RRR);
-	return (-1);
 }
 
 static int		issorted(t_pile *pile)
@@ -171,66 +165,7 @@ static int		issorted(t_pile *pile)
 	}
 	return (sorted);
 }
-
-static t_order	add(t_order map, char *order)
-{
-	t_order		*new;
-
-	new = (t_order *)malloc(sizeof(t_order));
-	if (new)
-	{
-		new->order = strton(order);
-		new->last = map.last;
-		new->next = NULL;
-		if (!map.next)
-			map.next = new;
-		if (new->last)
-			new->last->next = new;
-		map.last = new;
-	}
-	return (map);
-}
-
 /*
-void			loop(t_piles p, SDL_Window *win, SDL_Renderer *render)
-{
-	const Uint8		*keys = SDL_GetKeyboardState(NULL);
-	int				i;
-	unsigned int	count;
-	int				check;
-	char			*str;
-
-	p.dir = 2;
-	while (!SDL_QuitRequested())
-	{
-		handle_key(win, keys, &(p.dir), &(p.mov));
-		move(&(p.dst), &(p.mov));
-		anim(&(p.src), p.dir, keys);
-		ticks = SDL_GetTicks() + (1000 / FPS);
-		SDL_RenderCopy(render, p.tex, &(p.src), &(p.dst));
-		SDL_RenderPresent(render);
-		SDL_RenderClear(render);
-		while (SDL_GetTicks() < ticks);
-	}
-}
-*/
-
-static char		opposite(char order)
-{
-	if (order < 0 || order == SS)
-		return (order);
-	else if (order == RR || order == RRR)
-		return ((order == RR) ? RRR : RR);
-	else if (order <= PB)
-		return ((order == PA) ? PB : PA);
-	else if (order <= SB)
-		return ((order == SA) ? SB : SA);
-	else if (order <= RB)
-		return ((order == RA) ? RB : RA);
-	else
-		return ((order == RRA) ? RRB : RRA);
-}
-
 void			navigate(t_piles p, t_order map)
 {
 	Uint8	*keys;
@@ -248,44 +183,46 @@ void			navigate(t_piles p, t_order map)
 		autoplay = (keys[SDL_SCANCODE_SPACE] && !spaced) ? !autoplay : autoplay;
 		if ((keys[SDL_SCANCODE_LEFT] && cur->last) || (cur->next && (keys[SDL_SCANCODE_RIGHT] || autoplay)))
 		{
+			ft_printf("%s", strinstruct((keys[SDL_SCANCODE_LEFT]) ? opposite(cur->order) : cur->order));
+			apply(strinstruct((keys[SDL_SCANCODE_LEFT]) ? opposite(cur->order) : cur->order), p.p1, p.p2);
+			displaystatus(p);
 			if (keys[SDL_SCANCODE_LEFT] && cur->last)
 				cur = cur->last;
 			else if (cur->next)
 				cur = cur->next;
-			ft_printf("%s", strinstruct((keys[SDL_SCANCODE_LEFT]) ? opposite(cur->order) : cur->order));
-			apply(strinstruct((keys[SDL_SCANCODE_LEFT]) ? opposite(cur->order) : cur->order), p.p1, p.p2);
-			displaystatus(p);
 			SDL_Delay(1000/60);
 		}
 		spaced = keys[SDL_SCANCODE_SPACE];
 	}
 }
-
+*/
 int				visualize(t_piles p)
 {
-	t_order			map;
 	char			*str;
 	unsigned int	count;
+	unsigned int	max;
+	int				pop;
 	int				i;
 	int				check;
 
+	max = maxvalue(*(p.p1), &pop);
+	ft_printf("max = %u ; pop = %d\n", max, pop);
 	count = 0;
 	check = 1;
-	map.next = NULL;
-	map.last = NULL;
 	if (init())
 		return (0);
-	displaystatus(p);
+	displaystatus(p, max, pop);
 	while (!SDL_QuitRequested() && check && get_next_line(0, &str) > 0 && ++count)
 	{
 		i = 0;
 		while (ft_isspace(str[i]))
 			i++;
 		check = apply(str + i, p.p1, p.p2);
-		map = add(map, str + i);
-		displaystatus(p);
+		displaystatus(p, max, pop);
+		SDL_Delay(WIN_H / pop);
 		free(str);
 	}
-	navigate(p, map);
+	while (!SDL_QuitRequested())
+		;
 	return ((!check) ? -1 : (*p.p2 == NULL && issorted(*p.p1)));
 }
